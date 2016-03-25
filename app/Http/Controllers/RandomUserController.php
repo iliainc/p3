bfcfec4d417c764aba885fa531f0c5d83d284c06
Ilya <inossov@fas.harvard.edu>
2016-03-25 02:01:02 +0000

    Testing user generation fields

--- app/Http/Controllers/RandomUserController.php
+++ app/Http/Controllers/RandomUserController.php
@@ -20,11 +20,21 @@ class RandomUserController extends Controller
 
         ]);
 
-        // $faker = Faker\Factory::create();
-         return \Faker\Name::name();
-
-        //return $faker->name;
-
-        // return 'Generating Random User!';
+        // return \Faker\Name::name();
+        // return \Faker\Internet::freeEmail($name = null);
+        // return \Faker\PhoneNumber::phoneNumber();
+        // return \Faker\PhoneNumber::safePhoneNumber();
+        // return \Faker\Address::city();
+        // return \Faker\Address::address();
+        // return \Faker\Address::streetName();
+         return \Faker\Address::streetAddress($includeSecondary = false);
+        // return \Faker\Address::city();
+        // return \Faker\Address::state();
+        // return \Faker\Address::stateAbbr();
+        // return \Faker\Address::zipCode();
+        // return \Faker\Address::zip();
+        // return \Faker\Address::postcode();
+        // return \Faker\Address::cityStateZip();
+        // return \Faker\Address::country();
     }
 }
